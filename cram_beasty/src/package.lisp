bec0551f793b7cc434c24943b684c34d462bc8e4
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2014-01-07 10:46:43 +0100

    Also exported symbol of class beasty-interface.

--- cram_beasty/src/package.lisp
+++ cram_beasty/src/package.lisp
@@ -33,8 +33,8 @@
         #:common-lisp)
   (:export 
    ;; top-level interaction with beasty interface
-   :make-beasty-interface :command-beasty :robot :state :release-beasty-safety
-   :safety-released-p :ensure-safety-released :motors-on-p
+   :make-beasty-interface :beasty-interface :command-beasty :robot :state
+   :release-beasty-safety :safety-released-p :ensure-safety-released :motors-on-p
    ;; modelling of LWR robot for beasty
    :beasty-robot :simulation-flag :motor-power :tool-configuration :base-configuration
    :beasty-base :base-transform :base-acceleration :beasty-tool :ee-transform
